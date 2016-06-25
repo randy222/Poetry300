d14ecba4bcd84e08eaf61f7373794c3333ddc513
Randy <randy@randyde-macbook-pro.local>
2016-06-25 17:36:12 +0800

    Initial Commit

---
+++ Poetry300/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poetry300
+//
+//  Created by Randy on 2016/6/25.
+//  Copyright © 2016年 Randy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
